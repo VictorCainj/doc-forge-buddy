8277e7b12231988e7ea0f88ab5df5577eaf98730
victorcain <cainbrasil23@gmail.com>
2025-09-17 23:11:55 -0300

    feat: Atualizar configurações de build e otimizar o Vite
    - Alterar o comando de build no vercel.json para usar "npm run build" em vez de "npm run vercel-build"
    - Remover o script "vercel-build" do package.json
    - Adicionar configuração de servidor no vite.config.ts para definir host e porta em modo de desenvolvimento
    - Simplificar a estrutura de rewrites no vercel.json para redirecionar todas as requisições para index.html

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   resolve: {
     alias: {
@@ -13,4 +13,11 @@ export default defineConfig({
   build: {
     chunkSizeWarningLimit: 1000,
   },
-});
+  server:
+    mode === 'development'
+      ? {
+          host: '::',
+          port: 8080,
+        }
+      : undefined,
+}));
